14be6b3eaf97386cf0adc0ad24fc777141990dd4
Svetlana <bel-svetik@yandex.ru>
2024-06-08 01:55:20 +0300

    Выполнить ДЗ №1 Модуль 8

--- homework08-1.py
+++ homework08-1.py
@@ -13,12 +13,9 @@ def add_everything_up(a, b):
     except TypeError:
         return str(a) + str(b)
     except BaseException as exp_:
-        print(f'Понятия не имею, что за ошибка произошла. Разбирайтесь сами.\n'
+        print(f'Понятия не имею какая тут могла ошибка произойти?!! Разбирайтесь сами!\n'
               f'Вот вам данные ошибки: {exp_.__class__}, {exp_.args}')
-
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-
-
